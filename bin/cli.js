4d3025a7429be7811d65d03abddb02b747598d81
Marc H. Weiner <mhweiner234@gmail.com>
2024-12-20 15:50:38 -0500

    fix: add backwards compatibility for brek/lambdaconf cli, ie "brek" without any args still writes typedef file without having to specify command "write"

--- bin/cli.js
+++ bin/cli.js
@@ -4,16 +4,13 @@ const cmd = process.argv[2];
 
 switch (cmd) {
 
-    case 'generate-type':
-        // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
-        require('../dist/writeTypeDef').writeTypeDef();
-        break;
     case 'load':
         // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
         require('../dist/index').loadConf().then(() => console.log('Brek: Config loaded and written to conf.json'));
         break;
     default:
-        console.log(`Unknown command: ${cmd}`);
+        // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
+        require('../dist/writeTypeDef').writeTypeDef();
         break;
 
 }
