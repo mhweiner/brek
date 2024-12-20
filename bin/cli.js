ee870b1cbf17633599b6d59956952e6204285e3d
Marc H. Weiner <mhweiner234@gmail.com>
2024-12-20 15:52:50 -0500

    fix: make more clear the cli arguments

--- bin/cli.js
+++ bin/cli.js
@@ -4,13 +4,13 @@ const cmd = process.argv[2];
 
 switch (cmd) {
 
-    case 'load':
+    case 'load-conf':
         // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
         require('../dist/index').loadConf().then(() => console.log('Brek: Config loaded and written to conf.json'));
         break;
+    case 'generate-type':
     default:
         // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
         require('../dist/writeTypeDef').writeTypeDef();
-        break;
 
 }
